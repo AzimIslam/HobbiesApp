5e8f2ff412b1d8f28511179d4e80ee54a25b166b
Rikhil <rikhilshah@hotmail.co.uk>
2021-12-04 19:27:32 +0000

    removed blank=true from email fields

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+# Create your models here.
 
 class User(AbstractUser):
     profile_pic = models.ImageField()
